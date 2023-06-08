c244018068eab9137dbda19ecee9ff39a4a5bc5c
DorTeri <dortayari@gmail.com>
2023-06-08 14:11:41 +0300

    Added button cmp

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,9 +1,50 @@
-import { Text } from "react-native"
+import { useLayoutEffect } from "react"
+import { StyleSheet, View } from "react-native"
+import IconButton from "../cmps/UI/IconButton"
+import { GlobalStyles } from "../constants/styles"
 
-function ManageExpense() {
+function ManageExpense({ route, navigation }) {
+
+    const editedExpenseId = route.params?.expenseId
+    const isEditing = !!editedExpenseId
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: isEditing ? 'Edit Expense' : 'Add Expense'
+        })
+    }, [navigation, isEditing])
+
+    function onRemoveExpense() {
+
+    }
+
     return (
-        <Text>ManageExpense Screen</Text>
+        <View style={styles.container}>
+            
+            {isEditing &&
+                <View style={styles.removeContainer}>
+                    <IconButton icon='trash'
+                        color={GlobalStyles.colors.error500} size={36}
+                        onPress={onRemoveExpense} />
+                </View>
+            }
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary800
+    },
+    removeContainer: {
+        marginTop: 16,
+        paddingTop: 8,
+        borderTopWidth: 2,
+        borderTopColor: GlobalStyles.colors.primary200,
+        alignItems: 'center'
+    }
+})
 
 export default ManageExpense
