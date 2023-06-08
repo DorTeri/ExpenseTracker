d3d59c354d7d7e522ee70aba189d66ecbdc4b023
DorTeri <dortayari@gmail.com>
2023-06-08 13:33:42 +0300

    Added style and utils

--- cmps/ExpensesOutput/ExpensesOutput.js
+++ cmps/ExpensesOutput/ExpensesOutput.js
@@ -1,6 +1,7 @@
-import { View } from "react-native"
+import { StyleSheet, View } from "react-native"
 import ExpensesSummary from "./ExpensesSummary"
 import ExpensesList from "./ExpensesList"
+import { GlobalStyles } from "../../constants/styles"
 
 const EXPENSES = [
     {
@@ -26,16 +27,50 @@ const EXPENSES = [
         description: 'Another book',
         amount: 39.99,
         date: new Date('2023-01-19')
+    },
+    {
+        id: 'e5',
+        description: 'A pair of shoes',
+        amount: 59.99,
+        date: new Date('2021-12-19')
+    },
+    {
+        id: 'e6',
+        description: 'A pair of trousers',
+        amount: 34.99,
+        date: new Date('2023-12-05')
+    },
+    {
+        id: 'e7',
+        description: 'A book',
+        amount: 19.99,
+        date: new Date('2022-01-19')
+    },
+    {
+        id: 'e8',
+        description: 'Another book',
+        amount: 39.99,
+        date: new Date('2023-01-19')
     }
 ]
 
 function ExpensesOutput({ expenses , expensesPeriod }) {
     return (
-        <View>
+        <View style={styles.container}>
             <ExpensesSummary expenses={EXPENSES} periodName={expensesPeriod}/>
             <ExpensesList expenses={EXPENSES} />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: 24,
+        paddingBottom: 0,
+        backgroundColor: GlobalStyles.colors.primary700,
+    }
+})
 
 export default ExpensesOutput
